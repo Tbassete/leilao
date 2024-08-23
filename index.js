209177517915788ede3237cd679140658cf074c7
Thiago Bassete <taraujo438.ta@gmail.com>
2024-08-22 23:45:22 -0300

    started database

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const express = require("express");
+const Sequelize = require('sequelize');//conexao com mysql
 const app = express();
 
 const port = 3000;
+
+// conexao com o banco de dados mysql
+const Sequelize = new Sequelize('lance', 'root', 'jaguarasso',{
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+//criar a table no mysql
+const lance = db.sequelize.define('lance', {
+
+    leilao: {
+        type: db.Sequelize.int
+    },
+    lanceInicial: {
+        type: db.Sequelize.int
+    },
+    histLances: {
+        type: db.Sequelize.int
+    },
+    NumZap:{
+        type: db.Sequelize.int
+    },
+    nomeComprador:{
+        type: db.Sequelize.TEXT
+    }
+})
+//execute esse comando so uma vez para criar a table
+// lance.sync({force: true}) 
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
 
 app.get("/home", function(req, res){
     res.sendFile(__dirname+"/pag01.html")
@@ -12,6 +46,13 @@ app.get("/add", function(req, res){
     res.send("ta de sacanagi")
 })
 
+function darlance(){
+
+}
+
+// app.get("/darlance", function(req, res){
+    
+// })
 
 
 console.log("servidor rodando na porta 3000");
